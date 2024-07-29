875672e7ec8c4b5c3f781127c67bc9b6b0487939
Barbara <babillina@gmail.com>
2024-07-29 08:32:20 -0300

    Projeto githoje

--- app.js
+++ app.js
@@ -63,11 +63,11 @@ function reiniciarJogo() {
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    let numeroSecreto = gerarNumeroAleatorio();
+    let tentativas = 1;
 }
 
-let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
